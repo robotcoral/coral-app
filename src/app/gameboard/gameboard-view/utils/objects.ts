aea14b45f6d6baa2238282752eb6a3f8ddcb7a12
Samuel Höra <samuelhoera@gmail.com>
2021-08-16 19:15:25 +0200

    feat: added wireframe to slabs

--- src/app/gameboard/gameboard-view/utils/objects.ts
+++ src/app/gameboard/gameboard-view/utils/objects.ts
@@ -1,10 +1,31 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import {
+  BoxGeometry,
+  EdgesGeometry,
+  LineBasicMaterial,
+  LineSegments,
+  Mesh,
+  MeshBasicMaterial,
+} from 'three';
 
 export class Slab extends Mesh {
+  color: string;
+
   constructor(scale: number, color: string) {
     const geometry = new BoxGeometry(scale, scale / 2, scale);
     const material = new MeshBasicMaterial({ color });
     super(geometry, material);
+
+    this.color = color;
+
+    this.addWireframe(geometry);
+  }
+
+  private addWireframe(geometry: BoxGeometry) {
+    const edgesGeometry = new EdgesGeometry(geometry);
+    const edgesMaterial = new LineBasicMaterial({ color: 0x0, linewidth: 10 });
+    const wireframe = new LineSegments(edgesGeometry, edgesMaterial);
+    wireframe.renderOrder = 1;
+    this.add(wireframe);
   }
 }
 
