6343ddb5c97b86f324ceca82e43816a05d7f7175
hoersamu <samuelhoera@gmail.com>
2021-08-16 11:56:51 +0200

    feat: added color handeling

--- src/app/gameboard/gameboard-controls/gameboard-controls.component.ts
+++ src/app/gameboard/gameboard-controls/gameboard-controls.component.ts
@@ -1,15 +1,40 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-gameboard-controls',
   templateUrl: './gameboard-controls.component.html',
   styleUrls: ['./gameboard-controls.component.scss'],
 })
-export class GameboardControlsComponent {
+export class GameboardControlsComponent implements OnInit {
   @Output() move = new EventEmitter();
   @Output() rotate = new EventEmitter<number>();
 
   cube = true;
+  colors = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+  };
+  color = 'red';
+  colorExpanded = false;
+  style = '';
+
+  ngOnInit(): void {
+    window.onclick = function (event) {
+      if (!event.target.matches('.colorBtn')) {
+        var dropdowns = document.getElementsByClassName(
+          'color-dropdown-content'
+        );
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    };
+  }
 
   onMove() {
     this.move.emit(null);
@@ -17,5 +42,17 @@ export class GameboardControlsComponent {
 
   onRotate(dir = 1) {
     this.rotate.emit(dir);
+  }
+
+  onColorMenu() {
+    this.style = this.colorExpanded
+      ? ''
+      : 'position:absolute; margin-top:-6.75rem; background-color: lightgray;';
+    this.colorExpanded = !this.colorExpanded;
+  }
+
+  onColorSelect(color: string) {
+    this.color = color;
+    this.onColorMenu();
   }
 }
