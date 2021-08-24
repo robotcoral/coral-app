aa3d5624d9e2e9221df12c49952ce87078109358
hoersamu <samuelhoera@gmail.com>
2021-08-24 10:58:50 +0200

    feat: added proper resize handling

--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GameboardViewComponent } from './gameboard-view/gameboard-view.component';
 
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.scss'],
 })
-export class GameboardComponent {}
+export class GameboardComponent {
+  @ViewChild(GameboardViewComponent)
+  gameboardView: GameboardViewComponent;
+
+  onResize() {
+    this.gameboardView.onWindowResize();
+  }
+}
