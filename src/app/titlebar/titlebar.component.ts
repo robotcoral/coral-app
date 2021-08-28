b492bea188232fe27a2af368bb1ae6b956a3e057
Samuel Höra <samuelhoera@gmail.com>
2021-08-28 23:09:31 +0200

    feat: added settings modal workaround

--- src/app/titlebar/titlebar.component.ts
+++ src/app/titlebar/titlebar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SettingsModal } from '../common/modals';
+import { GameboardController } from '../gameboard/utils';
 
 @Component({
   selector: 'app-titlebar',
   templateUrl: './titlebar.component.html',
-  styleUrls: ['./titlebar.component.scss']
+  styleUrls: ['./titlebar.component.scss'],
 })
-export class TitlebarComponent implements OnInit {
+export class TitlebarComponent {
+  constructor(public controller: GameboardController) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openSettings() {
+    this.controller.openModal(SettingsModal);
   }
-
 }
