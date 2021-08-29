5e3f58bf69865cb511d960096123038f975cb264
Samuel Höra <samuelhoera@gmail.com>
2021-08-29 22:38:20 +0200

    refactor: removed ngx-codemirror6 (#84)

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
-import { basicSetup } from '@codemirror/basic-setup';
-import { EditorStateConfig } from '@robotcoral/ngx-codemirror6';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
+
+type EditorStateConfig = Parameters<typeof EditorState.create>[0];
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EditorComponent {
   sourceCode = '';
   config: EditorStateConfig = {
     extensions: [basicSetup],
   };
+  editor: EditorView;
+
+  @ViewChild('codemirrorhost') codemirrorhost: ElementRef = null;
+
+  ngAfterViewInit(): void {
+    this.config = this.config || {};
+    const state = EditorState.create(this.config);
+    this.init(state);
+  }
+
+  init(state: EditorState) {
+    this.editor = new EditorView({
+      state,
+      parent: this.codemirrorhost.nativeElement,
+    });
+  }
 }
