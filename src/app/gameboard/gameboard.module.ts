91bc5757a63fd74637464361bf1580f0335acd64
Samuel Höra <samuelhoera@gmail.com>
2021-08-30 22:00:59 +0200

    feat: added svg helper package

--- src/app/gameboard/gameboard.module.ts
+++ src/app/gameboard/gameboard.module.ts
@@ -1,17 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { GameboardControlsComponent } from './gameboard-controls/gameboard-controls.component';
 import { GameboardViewComponent } from './gameboard-view/gameboard-view.component';
 import { GameboardComponent } from './gameboard.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularSvgIconModule,
+  ],
   declarations: [
     GameboardComponent,
     GameboardControlsComponent,
     GameboardViewComponent,
   ],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [GameboardComponent],
 })
 export class GameboardModule {}
