4659e535ffc2c58317f9b25ad3669494cf138e64
robotcoralci <robotcoralci@users.noreply.github.com>
2021-09-16 19:21:07 +0000

    chore: bump version to 0.1.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Robot Coral Team'
 author = 'Robot Coral Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,4 +1,4 @@
 export const commonEnvironment = {
-  version: '0.1.7',
+  version: '0.1.8',
   worldFileVersion: 1,
 };
