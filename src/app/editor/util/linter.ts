aa0eebd9e1609f43df2f4199a8b3743c3aae0efd
robotcoralci <robotcoralci@users.noreply.github.com>
2021-11-06 18:31:24 +0000

    chore: run Prettier

--- src/app/editor/util/linter.ts
+++ src/app/editor/util/linter.ts
@@ -3,17 +3,17 @@ import { EditorView } from '@codemirror/view';
 import { compile } from '@robotcoral/lang-karol';
 
 export function lintKarol(view: EditorView): Diagnostic[] {
-    try {
-        compile(view.state.doc.toString());
-        return [];
-    } catch(err: any) {
-        return [
-            {
-                from: err.pos.from,
-                to: err.pos.to, 
-                severity: "error",
-                message: err.msg,
-            }
-        ];
-    }
+  try {
+    compile(view.state.doc.toString());
+    return [];
+  } catch (err: any) {
+    return [
+      {
+        from: err.pos.from,
+        to: err.pos.to,
+        severity: 'error',
+        message: err.msg,
+      },
+    ];
+  }
 }
